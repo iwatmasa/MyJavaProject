64e6d7de363f9132291d8a2008ed710064cccacc
iwatmasa <iwatmasa@fsi.co.jp>
2025-07-04 15:08:15 +0900

    Run Selenium test in headless mode for CI

--- src/test/java/com/example/SeleniumSampleTest.java
+++ src/test/java/com/example/SeleniumSampleTest.java
@@ -1,12 +1,14 @@
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class SeleniumSampleTest {
     @Test
     public void testOpenGoogle() {
-        // ChromeDriverのパスはGitHub Actionsでは自動でセットアップされます
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new"); // ヘッドレスモードで起動
+        WebDriver driver = new ChromeDriver(options);
         driver.get("https://www.google.com");
         // タイトルに"Google"が含まれているか確認
         assert driver.getTitle().contains("Google");
